e2539602cff93130cd6decf3de3ecf2da39c0e2f
Jorge Eduardo Perez Barrientos <Jorge Eduardo Perez Barrientos>
2016-02-16 10:29:49 -0500

    Simple example on how to implement a widget It has a button and each time is pressed, it generates a random number

---
+++ app/src/main/java/com/example/m14x/widgetexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.m14x.widgetexample;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
